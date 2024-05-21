974686c598644d9c85baa425e9268ca2c11a3ed6
Blanc <enrop.lkldt@slmails.com>
2024-05-20 22:47:35 -0400

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -354,20 +354,21 @@ function Defolder {
 
 # Auto disable power management on USB devices
 function Disable-PowerManagement {
-$hubs = Get-WmiObject Win32_Serialport | Select-Object Name,DeviceID,Description
-$powerMgmt = Get-WmiObject MSPower_DeviceEnable -Namespace root\wmi
-foreach ($p in $powerMgmt)
-{
- $IN = $p.InstanceName.ToUpper()
- foreach ($h in $hubs)
- {
-  $PNPDI = $h.PNPDeviceID
+    $hubs = Get-WmiObject Win32_Serialport | Select-Object Name,DeviceID,Description
+    $powerMgmt = Get-WmiObject MSPower_DeviceEnable -Namespace root\wmi
+    foreach ($p in $powerMgmt)
+    {
+     $IN = $p.InstanceName.ToUpper()
+     foreach ($h in $hubs)
+     {
+      $PNPDI = $h.PNPDeviceID
                 if ($IN -like "*$PNPDI*")
                 {
                      $p.enable = $False
                      $p.psbase.put()
                 }
- }
+     }
+    }
 }
 
 # Scoop Install
